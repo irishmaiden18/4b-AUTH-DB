0e0cccd9e77183c0fb5458c66abf173b6ac3ff04
irishmaiden18 <laurapohl18@gmail.com>
2026-03-26 12:14:09 -0400

    adds more basic set up

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
 // npm init -y
-
 // npm i express morgan mongoose dotenv bcrypt
 
+// import express
+const express = require('express');
+
+// import morgan
+const logger = require("morgan");
+
+// connect to MongoDB
+const connectToMongoDB = require("./database/connectToMongoDB")
+
+// set up the express app
+const app = express();
+
+// set up the port
+const PORT = 3000;
+
+// set up morgan
+app.use(logger("dev"))
+
+// format our express body
+app.use(express.json())
+
+
+// start listening
+app.listen(PORT, () => {
+    console.log(`Server is listening on Port: ${PORT}`)
+
+    // call the connectToMongoDB function
+    connectToMongoDB()
+})
